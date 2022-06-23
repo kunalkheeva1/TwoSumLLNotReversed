1a0193e9904c2168ecdef5a45f0e241f3ccfc2f9
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 01:42:48 -0700

    created a node class

--- src/TwoSumLLNotReversed.java
+++ src/TwoSumLLNotReversed.java
@@ -1,2 +1,17 @@
 public class TwoSumLLNotReversed {
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+            this.next =null;
+        }
+    }
+    //method to hv
+
+
+    public static void main(String[] args) {
+
+    }
 }
